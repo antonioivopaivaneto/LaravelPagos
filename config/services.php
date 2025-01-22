eb8d7321e1b40eb6ad5d9db164a2fc4c9cf871d3
junior <antonioivo.3@gmail.com>
2025-01-22 14:56:02 -0300

    currency conversion api ok

--- config/services.php
+++ config/services.php
@@ -18,6 +18,11 @@ return [
         'token' => env('POSTMARK_TOKEN'),
     ],
 
+    'currency_conversion' =>[
+        'base_uri' => env('CURRENCY_CONVERSION_BASE_URI'),
+        'api_key' => env('CURRENCY_CONVERSION_CLIENT_ID'),
+
+    ],
     'paypal' =>[
         'base_uri' => env('PAYPAL_BASE_URI'),
         'client_id' => env('PAYPAL_CLIENT_ID'),
